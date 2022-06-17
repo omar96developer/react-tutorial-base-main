a0ff0c0e5bc58cfd18eafc43adeeb082db221223
omar96developer <omakino@hotmail.it>
2022-06-17 18:28:50 +0200

    dark-mode app

--- dark-mode/inizio/src/App.js
+++ dark-mode/inizio/src/App.js
@@ -3,13 +3,41 @@ import data from "./data";
 import Articolo from "./Articolo";
 
 //Funzione che se presente 'Theme' nel localStorage
+const getFromLocalStorage = () => {
+  if(localStorage.getItem('theme')){
+    return localStorage.getItem('theme');
+  } 
+}
 // returna il suo valore o di default return 'light-mode'
 
 function App() {
+  const [theme, setTheme] = useState(getFromLocalStorage() || 'light-mode');
+
+  //function che cambia il tema a seconda del suo valore
+  const cambiaTema = () => {
+
+    if(theme === 'light-mode'){
+      setTheme('dark-mode')
+    }else{
+      setTheme('light-mode')
+    }
+  }
+  //AL MUTARE DEL THEME STATE,, ATTACCA UNA CLASSE AL NOSTO HTML TAG
+  useEffect(() => {
+    document.documentElement.className = theme;
+    localStorage.setItem('theme', theme);
+  },[theme])
+
   return (
     <section className="section-center">
       <div className="container">
-        <h2>DARK MODE APP</h2>
+        
+        <button className="btn " onClick={cambiaTema}>Cambia</button>
+        <section className="article-section">
+          {
+            data.map(el => <Articolo key={el.id} {...el}/>)
+          }
+        </section>
       </div>
     </section>
   );
