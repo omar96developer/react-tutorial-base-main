374a659f998f6cdc49b623a7aa187b5b05e83926
omar96developer <omakino@hotmail.it>
2022-06-18 22:23:33 +0200

    gelateria

--- gelateria/inizio/src/components/Gelato.js
+++ gelateria/inizio/src/components/Gelato.js
@@ -1,7 +1,26 @@
 import React from "react";
 
-const Gelato = () => {
-  return <article className="gelato">Un Gelato</article>;
+const Gelato = ({nome, decrizione, img, prezzo, categoria}) => {
+ 
+  return (
+    <article className="gelato">
+      <div className="img-container">
+        <img src={img} alt={nome} className="img" />
+      </div>
+      <div className="prd-info">
+        <header className="prd-header">
+          <div>
+            <h5>{nome}</h5>
+            <h6>{categoria}</h6>
+          </div>
+          <span className="prd-prezzo">
+            <h6>{(prezzo / 100).toFixed(2)}€</h6>
+          </span>
+        </header>
+        <p>{decrizione}</p>
+      </div>
+    </article>
+  );
 };
 
 export default Gelato;
